b27c3e1bccf31356f2e97366682724ced0c8c035
shreeji0312 <55413201+shreeji0312@users.noreply.github.com>
2020-04-25 19:13:19 -0400

    Button
    Adding button, Children and child concept . <everything is widget in flutter.>

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,27 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My first App'),
+          appBar: AppBar(
+            title: Text('My first App'),
+          ),
+          body: Column(
+            children: [
+              Text('This is body part!'),
+              RaisedButton(
+                child: Text('Answer1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer2'),
+                onPressed: null
+              ),
+              RaisedButton(
+                child: Text('Answer3'),
+                onPressed: null
+              ),
+            ]
+          )
         ),
-        body: Text('This is body part!'),
-      ),
     );
   }
 }

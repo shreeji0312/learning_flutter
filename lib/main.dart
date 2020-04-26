5a307711b7b790e418b2b07b5bdd49bf0aaf0377
shreeji0312 <55413201+shreeji0312@users.noreply.github.com>
2020-04-25 20:56:48 -0400

    stateful
    converting stateless to statefull widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,28 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
 
-  void answeredQue(){
-    questionIndex = questionIndex + 1;
+  void answeredQue() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
     print('Answer chosen!');
   }
-  void answerQuestion(){
+
+  void answerQuestion() {
     print('Answer chosen!');
   }
+
   Widget build(BuildContext context) {
     var questions = [
       'What\'s your favorite color?',
@@ -24,26 +36,23 @@ class MyApp extends StatelessWidget {
           appBar: AppBar(
             title: Text('My first App'),
           ),
-          body: Column(
-            children: [
-              Text(questions[questionIndex]),
-              RaisedButton(
-                child: Text('Answer1'),
-                onPressed: answerQuestion,
-              ),
-              RaisedButton(
-                child: Text('Answer2'),
-                onPressed: (){
-                  print('Answer2 is selected');
-                },
-              ),
-              RaisedButton(
-                child: Text('Answer3'),
-                onPressed: () => print('Answer3 is selected'),
-              ),
-            ]
-          )
-        ),
+          body: Column(children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              child: Text('Answer1'),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer2'),
+              onPressed: () {
+                print('Answer2 is selected');
+              },
+            ),
+            RaisedButton(
+              child: Text('Answer3'),
+              onPressed: () => print('Answer3 is selected'),
+            ),
+          ])),
     );
   }
 }
